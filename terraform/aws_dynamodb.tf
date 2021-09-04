145f6047b253aa77cce303c948c5a7a0c9a4e689
Edvinas Janusevicius <edvinas.janusevicius@method-analytics.com>
2021-09-04 12:38:04 +0100

    Deployment:
    * Bugfixed the "aws_lambda_invocation" resource
    
    * Added an "aws_s3_bucket" resource to host the front-end.
    
    * Added StadiumTableName parameters to the Terraform and AWS SAM templates.
    
    * Added an output message that tells the status of the stadium location provisioning using a Terraform output.
    
    * Added required variables to spin-up the resources above.
    
    * Created a AWS IAM Policy in the AWS SAM template.
    
    * Bugfixed "DynamoDB" class to use table name that is passed as a parameter name through Terraform.

--- terraform/aws_dynamodb.tf
+++ terraform/aws_dynamodb.tf
@@ -18,10 +18,10 @@ resource "aws_dynamodb_table" "stadium_location_table" {
 data "aws_lambda_invocation" "load_stadium_locations_to_table" {
   depends_on = [
     aws_dynamodb_table.stadium_location_table,
-    aws_cloudformation_export.premier_league_crime_api_sam_stack
+    aws_cloudformation_stack.premier_league_crime_api_sam_stack
   ]
 
-  function_name = aws_cloudformation_export.provisioning_lambda_name.value
+  function_name = data.aws_cloudformation_export.provisioning_lambda_name.value
   input = <<JSON
 {}
 JSON
